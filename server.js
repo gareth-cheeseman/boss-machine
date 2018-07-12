6c06928929d180d4db3b72f609e1bb26e11031f6
gareth-cheeseman <garethcheeseman@humbug.local>
2018-07-12 22:00:13 +0100

    Adds cors boilerplate

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 module.exports = app;
 
@@ -9,17 +10,14 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
-
+app.use(cors());
 
 // Add middware for parsing request bodies here:
-
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
 
-
 // This conditional is here for testing purposes:
-if (!module.parent) { 
+if (!module.parent) {
   // Add your code to start the server listening at PORT below:
-
 }
