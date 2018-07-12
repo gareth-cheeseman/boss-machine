c80606d5470e9b53ccd7d9922520da8938f8f441
gareth-cheeseman <garethcheeseman@humbug.local>
2018-07-12 22:05:58 +0100

    Completes boilerplate

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 
 module.exports = app;
 
@@ -10,14 +9,21 @@ module.exports = app;
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
+const cors = require('cors');
 app.use(cors());
 
 // Add middware for parsing request bodies here:
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
 const apiRouter = require('./server/api');
+apiRouter.use('/api', apiRouter);
 
 // This conditional is here for testing purposes:
 if (!module.parent) {
   // Add your code to start the server listening at PORT below:
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
 }
